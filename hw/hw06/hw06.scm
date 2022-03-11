79e965fff63ae04680985859bf50436488106c25
ycycycy-1 <574479389@qq.com>
2022-03-11 15:04:23 +0800

    hw06

--- hw/hw06/hw06.scm
+++ hw/hw06/hw06.scm
@@ -1,11 +1,32 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) 
+    (if (null? (cdr s))
+        null 
+        (car (cdr s))
+    )
+)
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) 
+    (if (null? (cddr s))
+        null 
+        (car (cddr s))
+    )
+)
 
-(define (sign val) 'YOUR-CODE-HERE)
+(define (sign val) 
+    (cond((= 0 val) 0)
+         ((> 0 val) -1)
+         (#t 1)
+    )
+)
 
 (define (square x) (* x x))
+;qucik pow by recusive
+(define (pow base exp) 
+    (cond ((= 0 exp) 1)
+          ((odd? exp) (* base (pow (square base) (quotient exp 2))))
+          ( (even? exp) ( pow (square base) (quotient exp 2) ) )
+    )
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+)
